f739d41827e7dceca05ab810634324cf233414bc
Sammy Kroner <sammyk118@gmail.com>
2021-02-02 21:25:28 -0800

    jawsdb statement

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db",
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burger_db",
+    });
+}
 
-connection.connect((err) => {
-    if (err) {
-        console.error(`error connectiong: ${err.stack}`);
-        return;
-    }
-    console.log(`connected as id ${connection.threadId}`);
-});
+connection.connect();
 
 module.exports = connection;
